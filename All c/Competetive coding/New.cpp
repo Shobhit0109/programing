b92f54f2ef230537f2273e967b13d13178cebc18
Shobhit0109 <shobhit.ug21@nsut.ac.in>
2022-06-30 12:05:00 +0530

    Thu 30 Jun 2022 12:05:00 PM IST

--- All c/Competetive coding/New.cpp
+++ All c/Competetive coding/New.cpp
@@ -14,9 +14,9 @@ int main(int argc,const char **argv) {
     if (argc > 1) cout << "\n\tYou have arguments also!!";
     else cout << "\n\tYou have no arguments!!";
 
-// To check:
-//[6/28/2022, 2:16:06 AM] For C source files, IntelliSenseMode was changed from "linux-gcc-x64" to "linux-clang-x64" based on compiler args and querying compilerPath: "/usr/bin/clang"
-//[6/28/2022, 2:16:06 AM] IntelliSenseMode was changed because it didn't match the detected compiler.  Consider setting "compilerPath" instead.  Set "compilerPath" to "" to disable detection of system includes and defines.
-
+/* 
+    cin >> (char*) argv; // == argv[0]
+    cout << (char*) argv;
+ */
  return 0;
 }
