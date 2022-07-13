9c4da2e7a328bd5b333720d0db0fbef4e84141bc
Shobhit0109 <shobhit.ug21@nsut.ac.in>
2022-07-14 01:29:07 +0530

    Thu 14 Jul 2022 01:29:06 AM IST

--- All c/c++/Coding 2/New.cpp
+++ All c/c++/Coding 2/New.cpp
@@ -6,9 +6,29 @@ using namespace std;
 #define i(i,j,n) ((i)*(n) + (j))
 #define endl ("\n")
 
+void printSubByRec(const int *a,const int n) {
+    if (n == 1) {
+        cout << *a << " ";
+        return;
+    }
+    
+
+}
 
 int main(void) {
 
+    // int size;
+    // cout << "\n\tEnter the number of elements : ";
+    // cin >> size;
+
+    // int arr[size];
+    // f(i,size) {
+    //     cout << "\n\tEnter the element " << i+1 << " : ";
+    //     cin >> arr[i];
+    // }
+
+    int size = 3,arr[3] = {15,20,12};
+    printSubByRec(arr, size);
 
  return 0;
 }
