5f1d00e96beed49715b15abae210ccb31c5b7511
Shobhit0109 <shobhit.ug21@nsut.ac.in>
2022-07-24 20:43:13 +0530

    Sun 24 Jul 2022 08:43:11 PM IST

--- All c/c++/Coding 2/New.cpp
+++ All c/c++/Coding 2/New.cpp
@@ -2,7 +2,7 @@
 #include <cstdio>
 using namespace std;
 
-#define f(i,n) for (int i = 0; i < (n); i++)
+#define f(i,a,n) for (int i = a; i < (n); i++)
 #define i(i,j,n) ((i)*(n) + (j))
 #define endl ("\n")
 
